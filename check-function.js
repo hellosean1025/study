589d3295db67a1903df349dfd26c1a38808d5ee4
苏文雄 <hellosean1025@163.com>
2017-11-16 06:03:16 -0600

    Update and rename isTextFile.js to check-function.js

--- check-function.js
+++ check-function.js
@@ -1,3 +1,10 @@
+export default function isPlainObject(obj) {
+  return obj ? typeof obj === 'object' && Object.getPrototypeOf(obj) === Object.prototype : false;
+}
+
+
+
+
 var fs = require( 'fs' );
 var char, buf;
 
